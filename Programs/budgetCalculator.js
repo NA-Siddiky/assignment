fba387f1a7dbfb9b348ad59208bafbdb6f3a5915
NA-Siddiky <siddiky.academic@gmail.com>
2021-01-22 03:02:26 +0600

    commenting programs

--- Programs/budgetCalculator.js
+++ Programs/budgetCalculator.js
@@ -2,9 +2,9 @@ function budgetCalculator(watch, phone, laptop) {
     var watch = (watch * 50);
     var phone = (phone * 100);
     var laptop = (laptop * 500);
-    var total = (watch + phone + laptop);
+    var total = (watch + phone + laptop);       //total budget for yours 3 item /
     return total;
 }
 
-var result = budgetCalculator(20, 5, 3);
+var result = budgetCalculator(20, 10, 2); 
 console.log(result);
--- Programs/hotelCost.js
+++ Programs/hotelCost.js
@@ -2,22 +2,22 @@ function hotelCost(days) {
     var cost = 0;
 
     if (days <= 10) {
-        cost = days * 100;
+        cost = days * 100;      // costing is 100 for first 10 days /
     }
     else if (days <= 20) {
         var first_10_Days = 10 * 100;
         var remaining = days - 10;
-        var secound_20_Days = remaining * 80;
+        var secound_20_Days = remaining * 80;       // costing is 80 for 11-20 days /
         cost = first_10_Days + secound_20_Days;
     }
     else {
         var first_10_Days = 10 * 100;
         var secound_20_Days = 10 * 80;
         var remaining = days - 20;
-        var rest_Of_Days = remaining * 50;
+        var rest_Of_Days = remaining * 50;      // costing is 50 for rest of days after 1-20 days /
         cost = first_10_Days + secound_20_Days + rest_Of_Days;
     }
     return cost;
 }
-var total_cost = hotelCost(100);
+var total_cost = hotelCost(40);
 console.log(total_cost);
